832c91e81dfcf907bdf7e3e9c6ebfa65d91c8d67
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-01-19 13:35:58 +0700

    customer-REST-service | initial commit

---
+++ src/main/java/com/ducdmd152/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.ducdmd152.springdemo.dao;
+
+import java.util.List;
+
+import com.ducdmd152.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
