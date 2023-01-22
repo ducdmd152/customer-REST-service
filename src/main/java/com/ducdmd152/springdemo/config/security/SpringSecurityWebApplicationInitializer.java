57ec8533c78a08b6ad36756e53ae36acab94dfcf
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-01-22 20:29:34 +0700

    Reset the repo

--- src/main/java/com/ducdmd152/springdemo/config/security/SpringSecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.ducdmd152.springdemo.config.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityWebApplicationInitializer 
-	extends AbstractSecurityWebApplicationInitializer {
-
-}
