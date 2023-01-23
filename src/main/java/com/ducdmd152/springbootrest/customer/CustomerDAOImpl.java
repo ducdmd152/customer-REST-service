508de4ed80123dd2ce19d74ee2f4b82f1000ee2f
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-01-23 09:20:06 +0700

    Spring JPA | Applied stand JPA api

--- src/main/java/com/ducdmd152/springbootrest/customer/CustomerDAOImpl.java
+++ src/main/java/com/ducdmd152/springbootrest/customer/CustomerDAOImpl.java
@@ -3,9 +3,10 @@ package com.ducdmd152.springbootrest.customer;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,45 +16,45 @@ public class CustomerDAOImpl implements CustomerDAO {
 	// need to inject the session factory
 	@Autowired
 	private EntityManager entityManager;
-			
+
 	@Override
 	public List<Customer> getCustomers() {
-		
+
 		// get the current hibernate session
-		Session currentSession = entityManager.unwrap(Session.class);
-				
-		// create a query  ... sort by last name
-		Query<Customer> theQuery = 
-				currentSession.createQuery("from Customer order by lastName",
-											Customer.class);
-		
+//		Session currentSession = entityManager.unwrap(Session.class);
+
+		// create a query ... sort by last name
+		TypedQuery<Customer> theQuery = entityManager.createQuery("from Customer order by lastName", Customer.class);
+
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
-				
-		// return the results		
+
+		// return the results
 		return customers;
 	}
 
 	@Override
 	public void saveCustomer(Customer theCustomer) {
-
+		System.out.println("Calling...");
 		// get current hibernate session
 		Session currentSession = entityManager.unwrap(Session.class);
-		
+
 		// save/upate the customer ... finally LOL
 		currentSession.saveOrUpdate(theCustomer);
-		
+//		entityManager.merge(theCustomer);
+
 	}
 
 	@Override
 	public Customer getCustomer(int theId) {
 
 		// get the current hibernate session
-		Session currentSession = entityManager.unwrap(Session.class);
-		
+//		Session currentSession = entityManager.unwrap(Session.class);
+
 		// now retrieve/read from database using the primary key
-		Customer theCustomer = currentSession.get(Customer.class, theId);
-		
+//		Customer theCustomer = currentSession.get(Customer.class, theId);
+		Customer theCustomer = entityManager.find(Customer.class, theId);
+
 		return theCustomer;
 	}
 
@@ -61,25 +62,18 @@ public class CustomerDAOImpl implements CustomerDAO {
 	public void deleteCustomer(int theId) {
 
 		// get the current hibernate session
-		Session currentSession = entityManager.unwrap(Session.class);
-		
+//		Session currentSession = entityManager.unwrap(Session.class);
+
 		// delete object with primary key
+//		Query theQuery = 
+//				currentSession.createQuery("delete from Customer where id=:customerId");
+//		theQuery.setParameter("customerId", theId);
+
 		Query theQuery = 
-				currentSession.createQuery("delete from Customer where id=:customerId");
+				entityManager.createQuery("delete from Customer where id=:customerId");
 		theQuery.setParameter("customerId", theId);
-		
-		theQuery.executeUpdate();		
+
+		theQuery.executeUpdate();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
