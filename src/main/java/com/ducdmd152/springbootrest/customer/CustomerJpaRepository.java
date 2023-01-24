9e05999e20ada303a6cf6dae443adfacf35f8fd0
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-01-24 10:03:45 +0700

    Spring Data REST | RestResourcePath configuaration

--- src/main/java/com/ducdmd152/springbootrest/customer/CustomerJpaRepository.java
+++ src/main/java/com/ducdmd152/springbootrest/customer/CustomerJpaRepository.java
@@ -1,7 +1,9 @@
 package com.ducdmd152.springbootrest.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="api/customers")
 public interface CustomerJpaRepository extends JpaRepository<Customer, Integer> {
 	// no need to write code!
 }
