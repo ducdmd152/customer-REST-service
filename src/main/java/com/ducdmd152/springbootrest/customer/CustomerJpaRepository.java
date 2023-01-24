0eac4f00acd1b7d6016538d1b4a06b761bcf2e10
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-01-24 10:07:05 +0700

    Reback | Remove RestResourcePath (endpoint) configuaration

--- src/main/java/com/ducdmd152/springbootrest/customer/CustomerJpaRepository.java
+++ src/main/java/com/ducdmd152/springbootrest/customer/CustomerJpaRepository.java
@@ -1,9 +1,7 @@
 package com.ducdmd152.springbootrest.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
 public interface CustomerJpaRepository extends JpaRepository<Customer, Integer> {
 	// no need to write code!
 }
